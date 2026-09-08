42d8588af0440eb79ec1f44d6b154d5f78ef44ee
John-Fronza <johnnafronza@gmail.com>
2026-09-08 11:52:54 -0300

    migrations feitas e alteração no diagrama

---
+++ biblioteca/database/migrations/2026_09_08_133410_create_livros_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('livros', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->string('autor');
+            $table->string('isbn')->nullable()->unique();
+            $table->string('categoria');
+            $table->smallInteger('ano_publicacao')->unsigned()->nullable();
+            $table->unsignedInteger('exemplares_totais');
+            $table->unsignedInteger('exemplares_disponiveis');
+            $table->text('descricao')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('livros');
+    }
+};
---
+++ biblioteca/database/migrations/2026_09_08_133418_create_emprestimos_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('emprestimos', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('livro_id');
+            $table->unsignedBigInteger('usuario_id');
+
+            $table->foreign('livro_id')
+                ->references('id')
+                ->on('livros')
+                ->restrictedOnDelete()
+                ->casacadeOnUpdate();
+
+            $table->foreign('usuario_id')
+                ->references('id')
+                ->on('usuarios')
+                ->restrictedOnDelete()
+                ->casacadeOnUpdate();
+
+            $table->date('data_emprestimo');
+            $table->date('data_devolucao_prevista');
+            $table->date('data_devolucao')->nullable();
+            $table->boolean('emprestado');
+            $table->text('observacoes');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('emprestimos');
+    }
+};
